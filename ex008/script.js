c85ff42fa779409acf4db3a1b61ea215f8be887e
kauana-sants <124754922+kauana-sants@users.noreply.github.com>
2024-04-25 14:59:02 -0300

    código

--- ex008/script.js
+++ ex008/script.js
@@ -29,19 +29,8 @@ class Pessoa{
 let pessoas = []
 
 const btn_add = document.getElementById("btn_add")
-const res = document.querySelector(".res")
+const res = document.querySelector("res")
 
-
-
-const addPessoa = ()=>{
-    res.innerHTML= ""
-    pessoas.map((p)=>{
-       const div = document.createElement("div")
-       div.setAttribute("class", "pessoa")
-       div.innerHTML =  `Nome: ${p.getNome()}  Idade: ${p.getIdade()}`
-       res.appendChild(div)
-    })
-}
 btn_add.addEventListener("click", (evt)=>{
     const nome = document.getElementById("f_nome")
     const idade = document.getElementById("f_idade")
@@ -58,10 +47,12 @@ btn_add.addEventListener("click", (evt)=>{
      console.log(pessoas)
 })
 
-
-/*
- const div = document.createElement("div")
-        div.setAttribute("class", "pessoa")
-        div.innerHTML = `Nome: ${p.getNome()}  Idade: ${p.getIdade()}`
-        res.appendChild(div)
-         */
+const addPessoa = ()=>{
+    res.innerHTML= ""
+    pessoas.map((p)=>{
+       const div = document.createElement("div")
+       div.setAttribute("class", "pessoa")
+       div.innerHTML =  `Nome: ${p.getNome()}  Idade: ${p.getIdade()}`
+       res.appendChild(div)
+    })
+}
