ddcd767a75183e310e1413fca71e1998a95ac3af
kauana-sants <124754922+kauana-sants@users.noreply.github.com>
2024-07-18 14:05:02 -0300

    código

--- ex008/script.js
+++ ex008/script.js
@@ -29,7 +29,7 @@ class Pessoa{
 let pessoas = []
 
 const btn_add = document.getElementById("btn_add")
-const res = document.querySelector("res")
+
 
 btn_add.addEventListener("click", (evt)=>{
     const nome = document.getElementById("f_nome")
@@ -47,12 +47,14 @@ btn_add.addEventListener("click", (evt)=>{
      console.log(pessoas)
 })
 
+const res = document.getElementsByClassName("res");
+
 const addPessoa = ()=>{
     res.innerHTML= ""
     pessoas.map((p)=>{
        const div = document.createElement("div")
        div.setAttribute("class", "pessoa")
-       div.innerHTML =  `Nome: ${p.getNome()}  Idade: ${p.getIdade()}`
-       res.appendChild(div)
+       document.div.innerHTML =  `Nome: ${p.getNome()}  Idade: ${p.getIdade()}`
+       res.appendChild(div);
     })
 }
